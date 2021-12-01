95895feb8d85c94c30d246e332688b327123bc68
Patrick de Souza Lima <patricksw94@gmail.com>
2021-12-01 13:16:33 -0300

    botão mySkills

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,72 +1,100 @@
-import React, { useState } from 'react';
-import { Text, StyleSheet, SafeAreaView, TextInput, Platform, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 
-export function Home(){
-    const [newSkill, setNewSkill] = useState('');
-    const [mySkills, setMySkills] = useState([])
+export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.titleText}>Welcome, Patrick</Text>
 
-      <TextInput 
+      <TextInput
         style={styles.input}
         placeholder="New skill"
         placeholderTextColor="#555"
         onChangeText={setNewSkill}
       />
 
-      <TouchableOpacity 
-      style={styles.button}
-      activeOpacity={.7}
-      >
-          <Text style={styles.buttonText}>Add</Text>
+      <TouchableOpacity
+       style={styles.button}
+        activeOpacity={0.7}
+        >
+
+        <Text style={styles.buttonText}
+        >Add
+        </Text>
+
       </TouchableOpacity>
 
       <Text style={styles.titleSkills}>
-          My Skills
+      My Skills
       </Text>
 
+      <TouchableOpacity style={styles.buttonSkill}>
+        <Text style={styles.textSkill}>
+            teste
+        </Text>
+      </TouchableOpacity>
+
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#121015',
-        paddingHorizontal: 30,
-        paddingVertical: 70
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 30,
+    paddingVertical: 70,
+  },
+  titleText: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  titleSkills: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 45
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  buttonSkill: {
+      backgroundColor: '#1F1E25',
+      padding: 15,
+      borderRadius: 50,
+      alignItems: 'center'
     },
-    titleText: {
-        color: '#fff',
-        fontSize: 24,
+    textSkill: {
+        color: '#FFF',
+        fontSize: 22,
         fontWeight: 'bold'
     },
-    titleSkills: {
-        color: '#fff',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 50
-    },
-    input: {
-        backgroundColor: '#1F1E25',
-        color: '#FFF',
-        fontSize: 18,
-        padding: Platform.OS === 'ios' ? 15 : 10,
-        marginTop: 30,
-        borderRadius: 7
-    },
-    button: {
-        backgroundColor: '#A370F7',
-        padding: 15,
-        borderRadius: 7,
-        alignItems: 'center',
-        marginTop: 20
-    },
-    buttonText: {
-        color: '#FFF',
-        fontSize: 17,
-        fontWeight: 'bold'
-    }
-})
+});
