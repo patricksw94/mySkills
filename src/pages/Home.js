5565e956af0abc2da49846668a3c821761b89911
Patrick de Souza Lima <patricksw94@gmail.com>
2021-12-01 12:45:46 -0300

    implementação do StyleSheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, StyleSheet, SafeAreaView } from 'react-native';
 
 export function Home(){
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>React Native</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Welcome, Patrick</Text>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#121015',
+        paddingHorizontal: 30,
+        paddingVertical: 70
+    },
+    title: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    }
+})
